a0b19a03ae456baf77a5879d6a7355c5ff1a3d6d
YakubinaOleg <akubinaoleh009@gmail.com>
2026-09-12 16:24:42 +0300

    Lab01 Task01

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lab01;
+using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+Task1.Run();
---
+++ Lab01/Task1.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab01;
+
+public static class Task1
+{
+    public static void Run()
+    {
+        Console.Write("Введіть вагу: ");
+        double weight = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Введіть зріст: ");
+        double height = double.Parse(Console.ReadLine()!);
+
+        double body_mass_index = weight / (height * height);
+
+        Console.WriteLine($"{body_mass_index:F2}");
+    }
+}
