917f4481ddb5eb7b73ca00d58f71c612c6d51cbe
YakubinaOleg <akubinaoleh009@gmail.com>
2026-09-18 19:18:09 +0300

    Lab02 Task08

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -6,5 +6,5 @@ Console.OutputEncoding = Encoding.UTF8;
 
 Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-Task7.Run();
+Task8.Run();
 
---
+++ Lab02/Task8.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab02
+{
+    internal class Task8
+    {
+        public static void Run()
+        {
+            int D = int.Parse(Console.ReadLine()!);
+            int W = int.Parse(Console.ReadLine()!);
+
+            int[,,] data = new int[D, W, 2];
+
+            for (int i = 0; i < D; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    data[i, j, 0] = int.Parse(Console.ReadLine()!); 
+                    data[i, j, 1] = int.Parse(Console.ReadLine()!); 
+                }
+            }
+
+            int maxTotal = 0;
+            int maxPatients = -1;
+
+            for (int i = 0; i < D; i++)
+            {
+                Console.WriteLine($"Відділення {i + 1}:");
+                int totalP = 0;
+
+                for (int j = 0; j < W; j++)
+                {
+                    int morning = data[i, j, 0];
+                    int evening = data[i, j, 1];
+                    int weekTotal = morning + evening;
+                    totalP += weekTotal;
+
+                    Console.WriteLine($"  Тиждень {j + 1}: ранок {morning}, вечір {evening} -> разом {weekTotal}");
+                }
+
+                Console.WriteLine($"  Разом: {totalP} пацієнтів");
+
+                if (totalP > maxPatients)
+                {
+                    maxPatients = totalP;
+                    maxTotal = i;
+                }
+            }
+
+            Console.WriteLine($"Найзавантаженіше: Відділення {maxTotal + 1} ({maxPatients} пацієнтів)");
+        }
+    }
+}
