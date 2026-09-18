f07e7907f160a2ed482d87e885e7f656f91f36c3
YakubinaOleg <akubinaoleh009@gmail.com>
2026-09-18 16:01:00 +0300

    Lab02 Task02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -6,5 +6,5 @@ Console.OutputEncoding = Encoding.UTF8;
 
 Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-Task1.Run();
+Task2.Run();
 
---
+++ Lab02/Task2.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab02;
+
+public static class Task2
+{
+    public static void Run()
+    {
+    int n = int.Parse(Console.ReadLine()!);
+    int[] prices = new int[n];
+    for (int i = 0; i < n; ++i)
+    {
+        prices[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    // Зберігаємо початковий стан
+    string before = string.Join(" ", prices);
+
+    // Сортування бульбашкою
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (prices[j] > prices[j + 1])
+            {
+                int temp = prices[j];
+                prices[j] = prices[j + 1];
+                prices[j + 1] = temp;
+            }
+        }
+    }
+
+    // Вивід одним рядком із переносами \n
+    Console.WriteLine($"Черга (до): {before}\nЧерга (після): {string.Join(" ", prices)}\nНайдешевший: {prices[0]} грн\nНайдорожчий: {prices[n - 1]} грн");
+    }
+}
