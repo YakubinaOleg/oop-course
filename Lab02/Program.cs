4cc88ffa367419b6b98c84316c4be11f195fe683
YakubinaOleg <akubinaoleh009@gmail.com>
2026-09-18 15:45:49 +0300

    Lab02 Task01

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lab02;
+using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+Task1.Run();
+
---
+++ Lab02/Task1.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lab02;
+
+public static class Task1
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        double[] patients = new double[n];
+        for (int i = 0; i < n; ++i)
+        {
+            patients[i] = double.Parse(Console.ReadLine()!);
+        }
+        double min = patients[0];
+        double max = patients[0];
+        double sum = 0;
+        foreach (double i in patients)
+        {
+            if (i < min)
+            {
+                min = i;
+            }
+            if (i > max) 
+            {
+                max = i;
+            }
+            sum += i;
+        }
+
+
+        int total = 0;
+        double average = sum / n;
+        foreach (double i in patients)
+        {
+            if (i > average)
+            {
+                total += 1;
+            }
+        }
+        Console.WriteLine($"Кількість: {n} / Середня вага: {average:F1} кг / Мін / Макс: {min:F1} / {max:F1} кг / Вище середнього: {total} з {n}");
+
+    }
+}
