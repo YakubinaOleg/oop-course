b3588b4695d9e1c2521f48af93bbb75bbf2d86d0
YakubinaOleg <akubinaoleh009@gmail.com>
2026-09-18 18:17:36 +0300

    Lab02 Task06

---
+++ Lab02/Task6.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Lab02
+{
+    internal class Task6
+    {
+        public static void Run()
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            int[][] price = new int[n][];
+
+            for (int i = 0; i < n; ++i)
+            {
+                int k = int.Parse(Console.ReadLine()!);
+                price[i] = new int[k];
+                for (int j = 0; j < k; ++j)
+                {
+                    price[i][j] = int.Parse(Console.ReadLine()!);
+                }
+            }
+            int maxIdx = 0;
+            int maxSum = -1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                int sum = 0;
+                for (int j = 0; j < price[i].Length; ++j)
+                {
+                    sum += price[i][j];
+                }
+                double avg = (double)sum / price[i].Length;
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    maxIdx = i;
+                }
+
+                Console.WriteLine($"Лікар {i + 1}: {price[i].Length} прийоми, сума={sum} грн, середня={avg:F2} грн");
+            }
+
+            Console.WriteLine($"Найбільший дохід: Лікар {maxIdx + 1} ({maxSum} грн)");
+        }
+    }
+}
